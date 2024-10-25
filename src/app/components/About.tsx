29d9b75574a3d65fcaf0dc288de5b9d83f8356d4
Joseph Macaraig <jbmacaraig14@gmail.com>
2024-10-24 17:55:12 -0700

    deploy

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -2,10 +2,6 @@
 
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
-import { FiGithub } from "react-icons/fi";
-import { FaInstagram } from "react-icons/fa";
-import { SlSocialLinkedin } from "react-icons/sl";
 import { motion } from "framer-motion";
 import { hobbies } from "../lib/data";
 
@@ -26,10 +22,10 @@ export default function About() {
         <div className="flex flex-col-reverse gap-4 sm:grid sm:grid-cols-2 mt-10">
           <div className="flex flex-col gap-4">
             <p className="text-justify">
-              I'm not just someone who writes code—I'm a problem solver, a
-              creative thinker, and a tech enthusiast. I love diving into
-              complex challenges and architecting solutions that deliver
-              exceptional user experiences. Whether it's crafting sleek
+              I&apos;m not just someone who writes code—I&apos;m a problem
+              solver, a creative thinker, and a tech enthusiast. I love diving
+              into complex challenges and architecting solutions that deliver
+              exceptional user experiences. Whether it&apos;s crafting sleek
               front-end interfaces or designing robust back-end systems, I
               thrive on the opportunity to blend creativity with technical
               expertise. But what truly drives me is the chance to collaborate
@@ -37,12 +33,12 @@ export default function About() {
             </p>{" "}
             <p className="text-justify">
               {" "}
-              In essence, I'm not just passionate about software development—I'm
-              passionate about the people, the projects, and the possibilities
-              that come with it. And I'm excited to embark on this journey of
-              continuous learning, growth, and innovation with like-minded
-              individuals who share my vision for building a better digital
-              future.
+              In essence, I&apos;m not just passionate about software
+              development—I&apos;m passionate about the people, the projects,
+              and the possibilities that come with it. And I&apos;m excited to
+              embark on this journey of continuous learning, growth, and
+              innovation with like-minded individuals who share my vision for
+              building a better digital future.
             </p>
             <div className="text-lg">
               <p className="mb-5 font-medium">Hobbies:</p>
@@ -72,7 +68,9 @@ export default function About() {
             {/* about me */}
             <div className="text-center">
               <h1 className="md:text-lg font-bold">Joseph Bryan</h1>
-              <p className="font-light md:text-lg">Full Stack Software Engineer</p>
+              <p className="font-light md:text-lg">
+                Full Stack Software Engineer
+              </p>
             </div>
           </div>
         </div>
